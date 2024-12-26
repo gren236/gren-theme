0a955900ff676b6d9d61c02d7b9c7ed7c3771430
Egor Rudkov <gren236@gmail.com>
2024-12-26 15:42:03 +0100

    Refactor navigation menu for improved usability and responsiveness, enhance header layout, and update social links styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const burger = document.querySelector(".burger");
+  const menuToggle = document.querySelector(".menu-toggle");
+  const burgerMenu = document.querySelector("#burger-menu");
+  const closeMenu = document.querySelector("#close-menu");
   const nav = document.querySelector(".nav-menu");
 
-  burger.addEventListener("click", () => {
-    nav.classList.toggle("active");
-  });
+  let isMenuOpen = false;
+
+  const toggleMenu = () => {
+    burgerMenu.classList.toggle("hidden");
+    closeMenu.classList.toggle("hidden");
+    nav.classList.toggle("hidden");
+    nav.classList.toggle("flex");
+
+    isMenuOpen = !isMenuOpen;
+  };
+
+  menuToggle.addEventListener("click", toggleMenu);
 
   // Close menu when clicking outside
   document.addEventListener("click", (e) => {
-    if (!nav.contains(e.target) && !burger.contains(e.target)) {
-      nav.classList.remove("active");
+    if (
+      !nav.contains(e.target) &&
+      !menuToggle.contains(e.target) &&
+      isMenuOpen
+    ) {
+      toggleMenu();
     }
   });
 });
