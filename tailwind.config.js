12ba75855503f64723456e47fdd46b14e4187e79
Egor Rudkov <gren236@gmail.com>
2024-12-27 02:03:42 +0100

    Add PostCSS configuration and update Tailwind CSS setup with new dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./content/**/*.{html,md}",
     "./layouts/**/*.html",
-    "./themes/**/layouts/**/*.html",
   ],
   theme: {
     colors: {
